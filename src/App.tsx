18e6fa011025f8ffe331ab8519b8c41af5648b7a
Pavel Glebik <g1ebikpasha@gmail.com>
2026-03-25 14:15:14 +0300

    api generation (openapi-typescript / openapi-fetch
    Signed-off-by: Pavel Glebik <g1ebikpasha@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react"
+import { client } from "./shared/api/client"
 
 function App() {
+
+  useEffect(() => {
+    (async function () {
+      const response = await client.GET('/playlists')
+      const data = response.data
+      console.log('data: ', data)
+    })()
+  }, [])
   return (
     <>
       React App
